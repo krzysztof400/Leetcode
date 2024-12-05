20aae38b53def0e992c91b8fd9bee691931932bf
Krzysztof Zając <157144045+krzysztof400@users.noreply.github.com>
2024-12-05 14:20:11 +0100

    Time: 164 ms (50.84%), Space: 107.8 MB (48.79%) - LeetHub

---
+++ 3351-maximize-happiness-of-selected-children/3351-maximize-happiness-of-selected-children.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    long long maximumHappinessSum(std::vector<int>& happiness, int k) {
+        std::sort(happiness.begin(), happiness.end(), std::greater<int>());
+        long long result = 0;
+
+        for (int i = 0; i < k; i++) {
+            result += std::max(0, happiness[i] - i);
+        }
+
+        return result;
+    }
+};
