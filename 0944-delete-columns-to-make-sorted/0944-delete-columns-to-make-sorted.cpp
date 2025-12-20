17c3b835d1b078cb2d14e9403acb1c318b73cfab
Krzysztof Zając <157144045+krzysztof400@users.noreply.github.com>
2025-12-20 12:05:49 +0100

    Time: 4 ms (45.13%), Space: 15.5 MB (45.93%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
@@ -5,7 +5,7 @@ public:
         size_t len_of_strings = strs[0].size();
         for(int i = 0; i < len_of_strings; i++){
             for(int j = 0; j<strs.size()-1; j++){
-                if((int)strs[j+1][i] - (int)strs[j][i] < 0){
+                if((int)strs[j+1][i] < (int)strs[j][i]){
                     count++;
                     break;
                 }
