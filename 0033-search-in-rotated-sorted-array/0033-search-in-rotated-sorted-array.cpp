1693df217748658534bafc46f8ab4a2a687bfbdb
Krzysztof Zając <157144045+krzysztof400@users.noreply.github.com>
2026-05-22 17:49:06 +0200

    Time: 0 ms (100%), Space: 15.2 MB (66.43%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -8,39 +8,31 @@ public:
         int idx = -1;
         while(right >= left) {
             mid = (left+right) / 2;
-            printf("right: %d, left: %d, mid: %d \n", right, left, mid);
+            // printf("right: %d, left: %d, mid: %d \n", right, left, mid);
             // found
             if(nums[mid] == target) {
                 idx = mid;
                 break;
             }
             else if (nums[mid] > target) {
-                // normal bs
+                // case where we chose diffrent half anyway
                 if (nums[right] >= target && nums[mid] > nums[right]) {
                     left = mid+1;
                 }
+                // normal bs
                 else {
                     right = mid-1;
                 }
-                // // our 
-                // else if (nums[left] < target) {
-                //     right = mid-1;
-                // }
             }
             else if (nums[mid] < target) {
-                // normal bs
+                // case where we chose diffrent half anyway
                 if (target >= nums[left] && nums[left] > nums[mid]) {
                     right = mid-1;
                 }
+                // normal bs
                 else {
                     left = mid+1;
                 }
-                // if (nums[mid] < nums[right] && nums[right] > target) {
-                //     left = mid+1;
-                // }
-                // else if(nums[right] < target) {
-                //     right = mid-1;
-                // }
             }
         }
         return idx;
