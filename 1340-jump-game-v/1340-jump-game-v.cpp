59b3bd8df03d5b99162cdb049b87ee4accea32d5
Krzysztof Zając <157144045+krzysztof400@users.noreply.github.com>
2026-05-24 12:43:23 +0200

    Time: 2026 ms (5.05%), Space: 49.8 MB (10.09%) - LeetHub

---
+++ 1340-jump-game-v/1340-jump-game-v.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int maxJumps(vector<int>& arr, int d) {
+        int n = arr.size();
+        vector<int> DP(n, 1);
+        
+        vector<int> indices(n, 0);
+        iota(indices.begin(), indices.end(), 0);
+        sort(indices.begin(), indices.end(), [&arr](const int& a, const int& b){
+            return arr[a] < arr[b];
+        });
+
+        for(int i = 0; i<n; ++i) {
+            vector<int> jumps;
+            for(int j = 1; j<=d && j+ indices[i] < n; ++j) {
+                if (arr[indices[i] + j] >= arr[indices[i]]) {
+                    break;
+                }
+                jumps.push_back(indices[i] + j);
+            }
+            for(int j = 1; j<=d && indices[i] - j >= 0; ++j) {
+                if (arr[indices[i] - j] >= arr[indices[i]]) {
+                    break;
+                }
+                jumps.push_back(indices[i] - j);
+            }
+            int max_jump = 0;
+            printf("\n %d:      ", i);
+            for (int j : jumps) {
+                printf("%d, ", j);
+                max_jump = max(max_jump, DP[j]);
+            }
+            DP[indices[i]] = 1 + max_jump;
+        }
+        return *max_element(DP.begin(), DP.end());
+    }
+};
