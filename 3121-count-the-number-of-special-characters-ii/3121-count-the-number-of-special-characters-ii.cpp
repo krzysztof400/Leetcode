8e43955d04a71530b68a43e26468d6959f21af96
Krzysztof Zając <157144045+krzysztof400@users.noreply.github.com>
2026-05-27 11:15:53 +0200

    Time: 77 ms (41.57%), Space: 24.1 MB (89.61%) - LeetHub

--- 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.cpp
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.cpp
@@ -1,25 +1,45 @@
 class Solution {
 public:
     int numberOfSpecialChars(string word) {
-        unordered_map<char, int> map;
-        for(int i = 0; i<word.length(); ++i) {
-            if (islower(word[i])){
-                map[word[i]] = i;
+        int map[26] = {0}; // 1 - found lowercase, wasnt upper case before 2 - found uppercase after lowercase 3 - found lowercase after first uppercase
+        for(char c : word) {
+            // if (map[c - 'a'] == 3){
+            //     continue;
+            // }
+            if (islower(c)){
+                if(map[c - 'a'] == 2 || map[c - 'a'] == 3) {
+                    map[c - 'a'] = 3;
+                }
+                else {
+                    map[c - 'a'] = 1;
+                }
+            } 
+            else {
+                if (map[c - 'A'] == 1 || map[c - 'A'] == 2) {
+                    map[c - 'A'] = 2;
+                }
+                else {
+                    map[c - 'A'] = 3;
+                }
             }
+
         }
         // for(const auto&[key, val] : map) {
         //     printf("key: %c, value: %d ", key, val);
         // }
         int count = 0;
-        for(int i = 0; i<word.length(); ++i) {
-            char c = word[i];
-            if(isupper(c) && map.find(tolower(c)) != map.end()){
-                if (map[tolower(word[i])] < i) {
-                    // printf("\n%c, %d", word[i], i);
-                    count++;
-                }
-                map[tolower(c)] = 200001;
-            }
+        // for(int i = 0; i<word.length(); ++i) {
+        //     char c = word[i];
+        //     if(isupper(c) && map.find(tolower(c)) != map.end()){
+        //         if (map[tolower(word[i])] < i) {
+        //             // printf("\n%c, %d", word[i], i);
+        //             count++;
+        //         }
+        //         map[tolower(c)] = 200001;
+        //     }
+        // }
+        for(int i : map) {
+            if(i == 2) { count++; }
         }
         return count;
     }
