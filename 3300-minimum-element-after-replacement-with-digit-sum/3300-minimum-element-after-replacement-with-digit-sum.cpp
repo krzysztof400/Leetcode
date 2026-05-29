3fee0438b7b24d2f9cf18f28b4ee1a7cfdf3c6aa
Krzysztof Zając <157144045+krzysztof400@users.noreply.github.com>
2026-05-29 15:56:07 +0200

    Time: 0 ms (100%), Space: 27.6 MB (77.43%) - LeetHub

--- 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.cpp
+++ 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int minElement(vector<int>& nums) {
-        int minim = 100;
+        int8_t minim = 100;
         for(int n : nums) {
-            int sum = 0;
+            int8_t sum = 0;
             while(n>0){
                 sum += n%10;
                 n /= 10;
